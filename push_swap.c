749cb311978d54ed592352b2a9785560877f09a7
Pau Botargues Julio <pbotargu@car13s2.42barcelona.com>
2023-11-29 12:27:41 +0100

    better

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 12:13:55 by pbotargu          #+#    #+#             */
-/*   Updated: 2023/11/22 12:57:03 by pbotargu         ###   ########.fr       */
+/*   Updated: 2023/11/29 12:24:30 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,13 @@ int main(int argc, char **argv)
     b = NULL;
     if (argc == 1 || argv[1][0] == '\0')
         return (1);
+}
+
+int main(int argc, char **argv)
+{
+    t_stack *a;
+
+    //iniciamos stack y comprobamos que no haya errorres.
+    ini_stack(t_stack **a, char **argv);
+    
 }
