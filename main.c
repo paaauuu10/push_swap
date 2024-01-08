3ac681d6907d95740776dfe3ceaf7c4f0ecef215
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2024-01-08 11:36:27 +0100

    main norminette ok

--- main.c
+++ main.c
@@ -12,31 +12,30 @@
 
 #include "push_swap.h"
 
-
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    t_stack *a;
-    t_stack *b;
+	t_stack	*a;
+	t_stack	*b;
 
-    a = NULL;
-    b = NULL; 
-    if (argc == 1)
-        return (1);
-    else if (argc == 2 && argv[1][0] == '\0')
-        return (1);
-    else if (argc == 2)
-        argv = ft_split(argv[1], ' ');
-    ft_init_stack(&a, argv, argc == 2);
-    if (!ft_is_sorted(a))
-    {
-        if (ft_stack_len(a) == 2)
-            sa(&a, 1);
-        else if (ft_stack_len(a) == 3)
-            ft_sort_three(&a);
-        else if (ft_stack_len(a) == 5)
-            ft_sort_five(&a, &b);
-        else
-            ft_push_swap(&a, &b);
-    }
-    ft_free_stack(&a);
+	a = NULL;
+	b = NULL;
+	if (argc == 1)
+		return (1);
+	else if (argc == 2 && argv[1][0] == '\0')
+		return (1);
+	else if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+	ft_init_stack(&a, argv, argc == 2);
+	if (!ft_is_sorted(a))
+	{
+		if (ft_stack_len(a) == 2)
+			sa(&a, 1);
+		else if (ft_stack_len(a) == 3)
+			ft_sort_three(&a);
+		else if (ft_stack_len(a) == 5)
+			ft_sort_five(&a, &b);
+		else
+			ft_push_swap(&a, &b);
+	}
+	ft_free_stack(&a);
 }
