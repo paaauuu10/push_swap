d52be2c0b66e94f8a361f26b3f37ce70e005e3c2
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2024-01-09 12:32:46 +0100

    sort_five norminette ok

--- sort_five.c
+++ sort_five.c
@@ -12,28 +12,28 @@
 
 #include "push_swap.h"
 
-void    ft_sort_five(t_stack **a, t_stack **b)
+void	ft_sort_five(t_stack **a, t_stack **b)
 {
-    t_stack *min_node;
-    int min_val;
-    int i;
+	t_stack	*min_node;
+	int		min_val;
+	int		i;
 
-    i = 0;
-    while (i < 2)
-    {
-        min_node = ft_find_small(*a);
-        min_val = min_node->value;
-        if (min_node == ft_find_last(*a))
-            rra(a, 1);
-        if ((*a)->value > min_val)
-            ra(a, 1);
-        else
-        {
-            pb(b, a);
-            i++;
-        }
-    }
-    ft_sort_three(a);
-    pa(a, b);
-    pa(a, b);
+	i = 0;
+	while (i < 2)
+	{
+		min_node = ft_find_small(*a);
+		min_val = min_node->value;
+		if (min_node == ft_find_last(*a))
+			rra(a, 1);
+		if ((*a)->value > min_val)
+			ra(a, 1);
+		else
+		{
+			pb(b, a);
+			i++;
+		}
+	}
+	ft_sort_three(a);
+	pa(a, b);
+	pa(a, b);
 }
