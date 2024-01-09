5f18d4e92ab9ebd0391072d810172dfb3585354a
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2024-01-09 12:46:24 +0100

    swap norminette ok

--- swap.c
+++ swap.c
@@ -12,39 +12,39 @@
 
 #include "push_swap.h"
 
-void    sa(t_stack **a, int flag)
+void	sa(t_stack **a, int flag)
 {
-    int aux;
+	int	aux;
 
-    if (*a == NULL || a == NULL)
-        return ;
-    if (ft_stack_len(*a) < 2)
-        return ;
-    aux = (*a)->value;
-    (*a)->value = (*a)->next->value;
-    (*a)->next->value = aux;
-    if (flag == 1)
-        write(1, "sa\n", 3);
+	if (*a == NULL || a == NULL)
+		return ;
+	if (ft_stack_len(*a) < 2)
+		return ;
+	aux = (*a)->value;
+	(*a)->value = (*a)->next->value;
+	(*a)->next->value = aux;
+	if (flag == 1)
+		write(1, "sa\n", 3);
 }
 
-void    sb(t_stack **b, int flag)
+void	sb(t_stack **b, int flag)
 {
-    int aux;
-    
-    if (*b == NULL || b == NULL)
-        return ;
-    if (ft_stack_len(*b) < 2)
-        return ;
-    aux = (*b)->value;
-    (*b)->value = (*b)->next->value;
-    (*b)->next->value = aux;
-    if (flag == 1)
-        write(1, "sb\n", 3);
+	int	aux;
+
+	if (*b == NULL || b == NULL)
+		return ;
+	if (ft_stack_len(*b) < 2)
+		return ;
+	aux = (*b)->value;
+	(*b)->value = (*b)->next->value;
+	(*b)->next->value = aux;
+	if (flag == 1)
+		write(1, "sb\n", 3);
 }
 
-void    ss(t_stack **a, t_stack **b)
+void	ss(t_stack **a, t_stack **b)
 {
-    sa(a, 0);
-    sb(b, 0);
-    write(1, "ss\n", 3);
+	sa(a, 0);
+	sb(b, 0);
+	write(1, "ss\n", 3);
 }
