16eddace7bff8c023a1bb45995362b4e894e8dfe
Pau Botargues Julio <paubj@MacBook-Air-de-Pau-4.local>
2024-01-09 12:21:26 +0100

    push_swap norminette ok

--- push_swap.c
+++ push_swap.c
@@ -12,81 +12,81 @@
 
 #include "push_swap.h"
 
-static void ft_rr_stacks(t_stack **a, t_stack **b, t_stack *cheapest)
+static void	ft_rr_stacks(t_stack **a, t_stack **b, t_stack *cheapest)
 {
-    while (*a != cheapest->target && *b != cheapest)
-        rr(a, b);
-    ft_current_position(*a);
-    ft_current_position(*b);
+	while (*a != cheapest->target && *b != cheapest)
+		rr(a, b);
+	ft_current_position(*a);
+	ft_current_position(*b);
 }
 
-static void ft_rrr_stacks(t_stack **a, t_stack **b, t_stack *cheapest)
+static void	ft_rrr_stacks(t_stack **a, t_stack **b, t_stack *cheapest)
 {
-    while (*a != cheapest->target && *b != cheapest)
-        rrr(a, b);
-    ft_current_position(*a);
-    ft_current_position(*b);
+	while (*a != cheapest->target && *b != cheapest)
+		rrr(a, b);
+	ft_current_position(*a);
+	ft_current_position(*b);
 }
 
-static void ft_put_top(t_stack **stack, t_stack *top, int a_b)
+static void	ft_put_top(t_stack **stack, t_stack *top, int a_b)
 {
-    while(*stack != top)
-    {
-        if (a_b == 1)
-        { 
-            if (top->above_median)
-                ra(stack, 1);
-            else
-                rra(stack, 1);
-        }
-        else if (a_b == 0)
-        {
-            if (top->above_median)
-                rb(stack, 1);
-            else
-                rrb(stack, 1);
-        }
-    }
-} 
+	while (*stack != top)
+	{
+		if (a_b == 1)
+		{
+			if (top->above_median)
+				ra(stack, 1);
+			else
+				rra(stack, 1);
+		}
+		else if (a_b == 0)
+		{
+			if (top->above_median)
+				rb(stack, 1);
+			else
+				rrb(stack, 1);
+		}
+	}
+}
 
-static void ft_move_nodes(t_stack **a, t_stack **b)
+static void	ft_move_nodes(t_stack **a, t_stack **b)
 {
-    t_stack *the_cheapest;
+	t_stack	*cheap;
 
-    the_cheapest = *b;
-    while (the_cheapest->cheapest == 0)
-        the_cheapest = the_cheapest->next;
-    if (the_cheapest->above_median && the_cheapest->target->above_median)
-        ft_rr_stacks(a, b, the_cheapest);
-    else if (!(the_cheapest->above_median) && !(the_cheapest->target->above_median))
-        ft_rrr_stacks(a, b, the_cheapest);
-    ft_put_top(a, the_cheapest->target, 1);
-    ft_put_top(b, the_cheapest, 0);
-    pa(a, b);
+	cheap = *b;
+	while (cheap->cheapest == 0)
+		cheap = cheap->next;
+	if (cheap->above_median && cheap->target->above_median)
+		ft_rr_stacks(a, b, cheapp);
+	else if (!(cheap->above_median) && !(cheap->target->above_median))
+		ft_rrr_stacks(a, b, cheap);
+	ft_put_top(a, cheap->target, 1);
+	ft_put_top(b, cheap, 0);
+	pa(a, b);
 }
 
 void	ft_push_swap(t_stack **a, t_stack **b)
 {
-    t_stack *smallest_one;
-    int len_a;
-    
-    len_a = ft_stack_len(*a);
-    while (len_a-- > 3)
-        pb(b, a);
-    ft_sort_three(a);
-    while(*b)
-    {
-        ft_info_nodes(*a, *b);
-        ft_move_nodes(a, b);
-    }
-    ft_current_position(*a);
-    smallest_one = ft_find_small(*a);
-    if (smallest_one->above_median)
-    {
-        while(*a != smallest_one)
-            ra(a, 1);
-    }
-    else
-        while (*a != smallest_one)
-            rra(a, 1);
+	t_stack	*smallest_one;
+	int		len_a;
+
+	len_a = ft_stack_len(*a);
+	while (len_a-- > 3)
+		pb(b, a);
+	ft_sort_three(a);
+	while (*b)
+	{
+		ft_info_nodes(*a, *b);
+		ft_move_nodes(a, b);
+	}
+	ft_current_position(*a);
+	smallest_one = ft_find_small(*a);
+	if (smallest_one->above_median)
+	{
+		while (*a != smallest_one)
+			ra(a, 1);
+	}
+	else
+		while (*a != smallest_one)
+			rra(a, 1);
 }
